9a307681f41b83ce35e6feef3e934dcfe5e82c19
Matias Paz <neoxap@gmail.com>
2023-03-06 12:07:52 -0300

    Commit final

--- javascript.js
+++ javascript.js
@@ -1,11 +1,33 @@
-console.log("Cargada la página para js")
+var iconosClass = [
+    "fa-solid fa-thumbs-up fa-5x",
+    "fa-solid fa-check fa-5x",
+    "fa-solid fa-comment fa-5x",
+    "fa-solid fa-question fa-5x",
+    "fa-solid fa-hand-point-up fa-5x",
+    "fa-solid fa-hand-peace fa-5x"
+]
 
-document.getElementById('foto').onclick = function () {
-console.log("Capturado el evento click a partir del click en foto");
+function iconoAvatar() {
+    var iconoRandom = Math.floor(Math.random() * iconosClass.length)
+    iconoAlAzar.setAttribute("class", `${iconosClass[iconoRandom]}`)
 }
 
-function myFunction() {
-    var x = document.getElementById("myDIV");
+document.getElementById('foto').onclick = function () {
+    iconoAvatar();
+    }
+
+
+function mostrarIdiomas() {
+    var x = document.getElementById("divIdiomas");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mostrarCodigo() {
+    var x = document.getElementById("divCodigo");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
