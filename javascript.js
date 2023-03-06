1c76ffdef8010a46f8652b6243857f8d59b9eecb
Matias Paz <neoxap@gmail.com>
2023-03-05 23:02:10 -0300

    Actualizaciones varias

--- javascript.js
+++ javascript.js
@@ -3,3 +3,12 @@ console.log("Cargada la página para js")
 document.getElementById('foto').onclick = function () {
 console.log("Capturado el evento click a partir del click en foto");
 }
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
